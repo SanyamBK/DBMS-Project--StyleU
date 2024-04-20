c79f212ad0dcd8f0fffd9f1f666ca9e6748f7fea
SanyamBK <119053140+SanyamBK@users.noreply.github.com>
2024-04-20 17:13:47 +0530

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -2,7 +2,7 @@ use styleu;
 
 DELIMITER //
 
-CREATE TRIGGER Block_User_After_Five_Attempts
+CREATE TRIGGER if not exists Block_User_After_Five_Attempts
 AFTER INSERT ON Login_Attempts
 FOR EACH ROW
 BEGIN
@@ -18,7 +18,30 @@ BEGIN
     
     -- If there are five or more failed attempts, update the user status
     IF attempts >= 5 THEN
-        UPDATE User_Details SET Status = 'Blocked' WHERE UserID = user_id;
+        UPDATE User_Details SET User_Status = 'Blocked' WHERE UserID = user_id;
+    END IF;
+END//
+
+DELIMITER ;
+
+
+DELIMITER //
+
+CREATE TRIGGER if not exists Update_Inventory_On_Low_Stock
+AFTER INSERT ON Order_User
+FOR EACH ROW
+BEGIN
+    DECLARE stock_count INT;
+    DECLARE product_id INT;
+
+    -- Get the product ID and current stock count
+    SELECT Quantity, ProductID INTO stock_count, product_id
+    FROM Inventory
+    WHERE ProductID = NEW.ProductID;
+
+    -- If the stock count is less than 10, add 30 to the inventory
+    IF stock_count < 10 THEN
+        UPDATE Inventory SET Quantity = Quantity + 30 WHERE ProductID = product_id;
     END IF;
 END//
 
